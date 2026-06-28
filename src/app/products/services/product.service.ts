bebcea6f6df850c08574faf1059b0cc3b66cf53e
Christopher <christophervc122@gmail.com>
2026-06-28 18:42:48 -0500

    feat: implement product fetching with options and add environment configuration

--- src/app/products/services/product.service.ts
+++ src/app/products/services/product.service.ts
@@ -2,14 +2,32 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { ProductsResponse } from '../interfaces/product.interface';
 import { Observable, tap } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+import { __param } from 'tslib';
+
+const baseUrl = environment.baseUrl;
+
+interface Options {
+  limit?: string;
+  offset?: string;
+  gender?: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
   private http = inject(HttpClient);
 
-  getProducts(): Observable<ProductsResponse> {
+  getProducts(options: Options): Observable<ProductsResponse> {
+    const { limit = 9, offset = 0, gender = '' } = options;
+
     return this.http
-      .get<ProductsResponse>('http://localhost:3000/api/products')
+      .get<ProductsResponse>(`${baseUrl}/products`, {
+        params: {
+          limit,
+          offset,
+          gender,
+        },
+      })
       .pipe(tap((resp) => console.log(resp)));
   }
 }
--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -14,7 +14,7 @@ export class HomePage {
   productsResource = rxResource({
     params: () => ({}),
     stream: ({ params }) => {
-      return this.productService.getProducts();
+      return this.productService.getProducts({ limit: '5', gender: 'women' });
     },
   });
 }

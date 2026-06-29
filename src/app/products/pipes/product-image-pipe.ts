0b2b48e7cd28832a1036b3640106e1b15b82ebe8
Christopher <christophervc122@gmail.com>
2026-06-28 19:51:10 -0500

    feat: implement ProductImagePipe for dynamic product image handling and add no-image placeholder

---
+++ src/app/products/pipes/product-image-pipe.ts
@@ -0,0 +1,33 @@
+import { Pipe, type PipeTransform } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+
+const baseUrl = environment.baseUrl;
+
+@Pipe({
+  name: 'ProductImage',
+})
+export class ProductImagePipe implements PipeTransform {
+  transform(value: string | string[]): string {
+    if (typeof value === 'string') {
+      return `${baseUrl}/files/product/${value}`;
+    }
+
+    const image = value?.at(0);
+
+    if (!image) {
+      return this.getImageUrlNotFound();
+    }
+
+    return this.getImageUrl(image);
+  }
+
+  private getImageUrl(image: string): string {
+    const imageBaseUrl = `${baseUrl}/files/product`;
+
+    return `${imageBaseUrl}/${image}`;
+  }
+
+  private getImageUrlNotFound(): string {
+    return '/assets/images/no-image.jpg';
+  }
+}

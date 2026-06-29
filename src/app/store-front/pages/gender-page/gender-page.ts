7b85a9903e2321dc354ffe55d6282f2c45ef5c80
Christopher <christophervc122@gmail.com>
2026-06-28 21:41:57 -0500

    feat: update gender page layout and implement product fetching logic

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { ProductCard } from '@/products/components/product-card/product-card';
+import { ProductService } from '@/products/services/product.service';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './gender-page.html',
 })
-export class GenderPage {}
+export class GenderPage {
+  route = inject(ActivatedRoute);
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+  productService = inject(ProductService);
+
+  productsResource = rxResource({
+    params: () => ({ gender: this.gender() }),
+    stream: ({ params }) => {
+      return this.productService.getProducts({
+        gender: params.gender,
+      });
+    },
+  });
+}

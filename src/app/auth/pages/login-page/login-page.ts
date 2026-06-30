14a1eaf003551785d83f9365b5accb89c0904fc0
Christopher <christophervc122@gmail.com>
2026-06-29 20:45:47 -0500

    feat: enhance login functionality with error handling and routing in AuthService

--- src/app/auth/pages/login-page/login-page.ts
+++ src/app/auth/pages/login-page/login-page.ts
@@ -1,5 +1,5 @@
 import { Component, inject, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '@/auth/services/auth.service';
 
@@ -9,12 +9,12 @@ import { AuthService } from '@/auth/services/auth.service';
   templateUrl: './login-page.html',
 })
 export class LoginPage {
-  
   fb = inject(FormBuilder);
   hasError = signal(false);
   isPosting = signal(false);
 
-  authService = inject(AuthService)
+  authService = inject(AuthService);
+  router = inject(Router);
 
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -31,8 +31,12 @@ export class LoginPage {
     }
     const { email = '', password = '' } = this.loginForm.value;
     console.log({ email, password });
-    this.authService.login(email!,password!).subscribe(resp => {
-      console.log(resp)
-    })
+    this.authService.login(email!, password!).subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        this.router.navigateByUrl('/');
+        return;
+      }
+      this.hasError.set(true);
+    });
   }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -3,7 +3,7 @@ import { User } from '../interfaces/user.interface';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment.development';
 import { AuthResponse } from '../interfaces/auth-response.interface';
-import { tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 
 const baseUrl = environment.baseUrl;
 
@@ -30,7 +30,7 @@ export class AuthService {
   user = computed(()=> this._user);
   token = computed(()=>this._token);
 
-  login(email: string, password: string){
+  login(email: string, password: string):Observable<boolean>{
     return this.http.post<AuthResponse>(`${baseUrl}/auth/login`,{
         email: email,
         password: password,
@@ -41,6 +41,13 @@ export class AuthService {
             this._token.set(resp.token)
 
             localStorage.setItem('token',resp.token)
+        }),
+        map(()=> true),
+        catchError((error: any) => {
+            this._user.set(null)
+            this._token.set(null)
+            this._authStatus.set('not-authenticated')
+            return of(false)
         })
     )
   }

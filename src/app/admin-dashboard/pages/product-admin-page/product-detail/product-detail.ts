4115462bfc71882cae2450b718c0f7e6f2fd08f7
Christopher <christophervc122@gmail.com>
2026-07-02 00:38:52 -0500

    feat: add form error label component and integrate validation feedback in product detail form

--- src/app/admin-dashboard/pages/product-admin-page/product-detail/product-detail.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-detail/product-detail.ts
@@ -1,61 +1,56 @@
 import { Product } from '@/products/interfaces/product.interface';
 import { Component, inject, input, OnInit } from '@angular/core';
-import { ProductCarousel } from "@/products/components/product-carousel/product-carousel";
+import { ProductCarousel } from '@/products/components/product-carousel/product-carousel';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormUtils } from '@/utils/form.utils';
+import { FormErrorLabel } from '@/shared/components/form-error-label/form-error-label';
 
 @Component({
   selector: 'product-detail',
-  imports: [ProductCarousel, ReactiveFormsModule],
+  imports: [ProductCarousel, ReactiveFormsModule, FormErrorLabel],
   templateUrl: './product-detail.html',
 })
 export class ProductDetail implements OnInit {
-  product = input.required<Product>()
+  product = input.required<Product>();
 
-  fb = inject(FormBuilder)
+  fb = inject(FormBuilder);
 
   productForm = this.fb.group({
     title: ['', Validators.required],
     description: ['', Validators.required],
-    slug: [
-      '',
-      [Validators.required, Validators.pattern(FormUtils.slugPattern)],
-    ],
+    slug: ['', [Validators.required, Validators.pattern(FormUtils.slugPattern)]],
     price: [0, [Validators.required, Validators.min(0)]],
     stock: [0, [Validators.required, Validators.min(0)]],
     sizes: [['']],
     images: [[]],
     tags: [''],
-    gender: [
-      'men',
-      [Validators.required, Validators.pattern(/men|women|kid|unisex/)],
-    ],
+    gender: ['men', [Validators.required, Validators.pattern(/men|women|kid|unisex/)]],
   });
 
-  sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL']
+  sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
 
-  ngOnInit(): void{
-    this.setFormValue(this.product())
+  ngOnInit(): void {
+    this.setFormValue(this.product());
   }
 
   setFormValue(formLike: Partial<Product>) {
-    this.productForm.reset(this.product() as any)
-    this.productForm.patchValue({ tags: formLike.tags?.join(',') })
+    this.productForm.reset(this.product() as any);
+    this.productForm.patchValue({ tags: formLike.tags?.join(',') });
   }
 
   onSizeClicked(size: string) {
-    const currentSizes = this.productForm.value.sizes ?? []
+    const currentSizes = this.productForm.value.sizes ?? [];
 
     if (currentSizes.includes(size)) {
-      currentSizes.splice(currentSizes.indexOf(size), 1)
+      currentSizes.splice(currentSizes.indexOf(size), 1);
     } else {
-      currentSizes.push(size)
+      currentSizes.push(size);
     }
-    this.productForm.patchValue({ sizes: currentSizes })
+    this.productForm.patchValue({ sizes: currentSizes });
   }
 
   onSubmit() {
-    console.log('Form submitted', this.productForm.value)
+    const isValid = this.productForm.valid;
+    console.log(this.productForm.value, isValid);
   }
-
 }

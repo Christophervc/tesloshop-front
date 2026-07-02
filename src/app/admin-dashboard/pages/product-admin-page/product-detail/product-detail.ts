1edddc06720d44074d15d187bf12ecf21195acc3
Christopher <christophervc122@gmail.com>
2026-07-01 22:30:08 -0500

    feat: enhance product detail form with dynamic gender buttons and size selection

--- src/app/admin-dashboard/pages/product-admin-page/product-detail/product-detail.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-detail/product-detail.ts
@@ -39,8 +39,19 @@ export class ProductDetail implements OnInit {
   }
 
   setFormValue(formLike: Partial<Product>) {
-    this.productForm.patchValue(formLike as any)
+    this.productForm.reset(this.product() as any)
     this.productForm.patchValue({ tags: formLike.tags?.join(',') })
+  }
+
+  onSizeClicked(size: string) {
+    const currentSizes = this.productForm.value.sizes ?? []
+
+    if (currentSizes.includes(size)) {
+      currentSizes.splice(currentSizes.indexOf(size), 1)
+    } else {
+      currentSizes.push(size)
+    }
+    this.productForm.patchValue({ sizes: currentSizes })
   }
 
   onSubmit() {
